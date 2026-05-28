c60b10c455bf3b941cf24ccc7b9e642ee2296be2
Muhammad Fayaz khan <128472503+muhammad-khan-epam@users.noreply.github.com>
2026-05-28 13:37:03 +0200

    remove cache pkg

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,6 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
