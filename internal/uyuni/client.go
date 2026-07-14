14d3a5bacc069dea82dee78252d73f52a30e8872
Muhammad Fayaz khan <128472503+muhammad-khan-epam@users.noreply.github.com>
2026-07-14 16:40:06 +0200

    fix the build endpoints

--- internal/uyuni/client.go
+++ internal/uyuni/client.go
@@ -1908,9 +1908,9 @@ func (c *Client) ListFilterCriteria(ctx context.Context) ([]FilterCriteriaOption
 }
 
 func (c *Client) BuildProject(ctx context.Context, projectLabel, message string) error {
-	_, err := apiPost[any](c, "contentmanagement/buildProject", map[string]any{
-		"projectLabel": projectLabel,
-		"message":      message,
+	endpoint := fmt.Sprintf("contentmanagement/projects/%s/build", url.QueryEscape(projectLabel))
+	_, err := apiPost[any](c, endpoint, map[string]any{
+		"message": message,
 	})
 	return err
 }
